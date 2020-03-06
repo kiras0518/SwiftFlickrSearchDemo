0e659cea89bd9dc8b775ceaa531247dfdb437ea4
kiras <kiras0518@gmail.com>
2020-03-06 18:38:34 +0800

    Initial Commit

---
+++ SwiftFlickrSearchDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFlickrSearchDemo
+//
+//  Created by kiras on 2020/3/6.
+//  Copyright © 2020 ameyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
